911e49e9138179679feac2ed911a9db7d7aecc29
camillecbf <miiykuu@gmail.com>
2023-09-26 17:36:43 +0200

    add pdf

---
+++ learning/main.js
@@ -0,0 +1,18 @@
+alert("etes vous sur de quitter cette page")
+
+console.log("bienvenue dans la console")
+
+var cardName = "Le chapeau de Serpentin"
+var age = 12
+var list = 
+
+const teacherAge = 28
+const studentAge = 17
+const teacherAgee = 25
+
+if (teacherAgee !== studentAge) {
+    console.log('the teacher is older than the student')
+}
+else {
+    console.log('the other is younger')
+}
